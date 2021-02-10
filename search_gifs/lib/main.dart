43bd40f5bfe9d8e87e5eec9bc374ce1eab02eba0
hilderlanherbert <hilderlan.herbert@gmail.com>
2021-02-09 21:22:51 -0300

    Completed app gif search

---
+++ search_gifs/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'package:search_gifs/ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(
+        hintColor: Colors.red,
+         primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+            enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.amber))
+        )),
+
+  ));
+
+}
+
+
---
+++ search_gifs/lib/ui/gif_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'package:share/share.dart';
+
+import 'package:search_gifs/ui/home_page.dart';
+
+class GifPage extends StatelessWidget {
+  final Map _gifData;
+
+  GifPage(this._gifData);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_gifData["title"]),
+        backgroundColor: Colors.red,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.share),
+            onPressed: (){
+              Share.share(_gifData["images"]["fixed_height"]["url"]);
+            },
+          )
+        ],
+      ),
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Image.network(_gifData["images"]["fixed_height"]["url"]),
+      ),
+    );
+  }
+}
